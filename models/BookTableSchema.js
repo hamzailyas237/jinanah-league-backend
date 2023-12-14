2d8d4b4ec5eb963fe3f8bf6c2fed423ad39e887a
hamzailyas237 <hamzailyas237@gmail.com>
2023-12-14 17:21:19 +0500

    added bookTableSchema

---
+++ models/BookTableSchema.js
@@ -0,0 +1,33 @@
+const { default: mongoose } = require("mongoose");
+
+const bookTableSchema = mongoose.Schema({
+  date: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  no_of_seats: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  products: {
+    type: Array,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  additional_info: {
+    type: String,
+  },
+});
+// const bookTable = mongoose.model('table', bookTableSchema)
+// module.exports = bookTable
